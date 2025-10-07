d2195a061a82cf81bf9ddc3e40a4a46718342fb7
GraisonMendozaRuiz <graisonmendoza@outlook.com>
2025-10-07 11:38:59 -0500

    fix: valid cors.

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,17 +1,21 @@
 export default [
-  'strapi::logger',
+  'strapi::errors',
   {
     name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://baguette-backend.onrender.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://baguette-backend.onrender.com'],
+        },
       },
     },
   },
-  'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
