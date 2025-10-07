5ed80bdc15cdcff875be42caafda4809d499bea5
GraisonMendozaRuiz <graisonmendoza@outlook.com>
2025-10-07 10:17:41 -0500

    fix: delete unused variables and install dependencies.

--- config/database.ts
+++ config/database.ts
@@ -25,10 +25,7 @@ export default ({ env }) => {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
