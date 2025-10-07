f3ec27bf96330dc1ff15f923bc88886cee349044
GraisonMendozaRuiz <graisonmendoza@outlook.com>
2025-10-07 11:14:07 -0500

    fix: cookie bug, again.

--- config/server.ts
+++ config/server.ts
@@ -1,18 +1,25 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://baguette-backend.onrender.com'),
+  url: env('STRAPI_ADMIN_BACKEND_URL', 'https://baguette-backend.onrender.com'),
   proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
   admin: {
     auth: {
-      secret: env('JWT_SECRET', '8TPRMXPNqXuQBxH0N//KDg==')
+      secret: env('JWT_SECRET', '8TPRMXPNqXuQBxH0N//KDg=='),
+      options: {
+        secure: true,
+        sameSite: 'none',
+      },
     },
-    cookies: {
-      secure: true,
-      sameSite: 'none'
-    }
-  }
+  },
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ['https://baguette-backend.onrender.com'],
+      credentials: true,
+    },
+  },
 });
