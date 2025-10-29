18d15daef141602f2976c0a4282dd2f9988a9fb1
GraisonMendozaRuiz <graisonmendoza@outlook.com>
2025-10-29 08:45:10 -0500

    feat: add service functions.

--- src/api/table/controllers/table.ts
+++ src/api/table/controllers/table.ts
@@ -2,30 +2,90 @@
  * table controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::table.table', ({ strapi }) => ({
-    async customAction (ctx) {
-        try {
-            const { status, message } = ctx.response;
-            const { data, meta } = await super.find(ctx)
-            ctx.body = { 
-                response: JSON.stringify(ctx.response),
-                state: JSON.stringify(ctx.state),
-                status: JSON.stringify(ctx.status),
-                success: status,
-                message,
-                data,
-                total: meta.pagination.total || 0,
-            }
-        } catch (err) {
-            ctx.body = err
-        }
-    },
-    async find(ctx) {
+export default factories.createCoreController(
+  "api::table.table",
+  ({ strapi }) => {
+    const service = strapi.service("api::menu-item.menu-item");
+    const DB = strapi.db.query("api::menu-item.menu-item");
+
+    return {
+      async find(ctx) {
         const { data, meta } = await super.find(ctx);
+        const { pagination } = meta;
         const { status, message } = ctx.response;
+
         meta.date = Date.now();
-        return { data, meta, status, message }
-    }
-}));
+        return { data, pagination, status, message };
+      },
+      async create(ctx) {
+        try {
+          const { data } = ctx.request.body;
+          if (!data) {
+            return ctx.badRequest("Empty body");
+          }
+
+          const entity = await service.create({ data });
+          const sanitized = await this.sanitizeOutput(entity, ctx);
+
+          ctx.body = {
+            success: true,
+            message: "Table created successfully!",
+            data: sanitized,
+          };
+        } catch (error) {
+          ctx.body = {
+            success: false,
+            message: error.message,
+          };
+        }
+      },
+      async edit(ctx) {
+        try {
+          const { data } = ctx.request.body;
+          if (!data || !data.id) {
+            return ctx.badRequest("Id required!");
+          }
+
+          const entity = await service.create({ data });
+          const sanitized = await this.sanitizeOutput(entity, ctx);
+
+          ctx.body = {
+            success: true,
+            message: "Table edited successfully!",
+            data: sanitized,
+          };
+        } catch (error) {
+          ctx.body = {
+            success: false,
+            message: error.message,
+          };
+        }
+      },
+      async delete(ctx) {
+        try {
+            const { id } = ctx.params;
+            const ID = parseInt(id, 10);
+
+            if (!id) return ctx.badRequest("id required!");
+
+            const found = await DB.findOne({
+                where: {id: ID},
+            })
+
+            if (!found) return ctx.notFound("Table doesn't exist")
+
+            await DB.delete({
+                where: { id: ID }
+            })
+        } catch (error) {
+            ctx.body = {
+                success: false,
+                message: error.message,
+            }
+        }
+      }
+    };
+  }
+);
--- src/api/table/routes/01-custom-table.ts
+++ src/api/table/routes/01-custom-table.ts
@@ -8,5 +8,29 @@ export default {
         auth: false,
       },
     },
+    {
+      method: "POST",
+      path: "/tables/create",
+      handler: "table.create",
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: "POST",
+      path: "/tables/edit",
+      handler: "table.edit",
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: "DELETE",
+      path: "/tables/delete/:id",
+      handler: "table.delete",
+      config: {
+        auth: false,
+      },
+    },
   ],
 };
